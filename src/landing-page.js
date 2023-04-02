e44067002dc6192bd5a97165e105ac94646e087f
Aniket Agrawal <aniket.mpa@gmail.com>
2023-04-02 16:47:45 +0200

    Add basic text to landing page

--- src/landing-page.js
+++ src/landing-page.js
@@ -1,8 +1,14 @@
 // fill content on landing page
+import "./landing-page.css";
+
 
 function createContent () {
-    let element = document.createElement("div.home");
-   
+    let intro = document.createElement("div");
+    intro.textContent = "Welcome to India - the land of delicious food, and amazing tastes. " +  
+                        "We bring you home, to the much-missed odours, to the familiar warmth " + 
+                        "of mom's cooking." ;
+    intro.style.fontFamily = "'Marck Script', cursive";
+    document.querySelector("#content").appendChild(intro);
 }
 
 export {createContent};
