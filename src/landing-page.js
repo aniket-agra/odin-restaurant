79aad9e9f54bf4435369989ba056aeb248068ed0
Aniket Agrawal <agra.aniket@gmail.com>
2023-04-03 17:10:21 +0200

    Add image to landing page

--- src/landing-page.js
+++ src/landing-page.js
@@ -1,14 +1,26 @@
 // fill content on landing page
 import "./landing-page.css";
-
+import Atithi from "../img/atithi.jpeg"
 
 function createContent () {
+    let content = document.querySelector("#content");
+    content.style.display = "flex";
+    content.style.alignItems = "center";
+    content.style.flexDirection = "column";
+    
     let intro = document.createElement("div");
     intro.textContent = "Welcome to India - the land of delicious food, and amazing tastes. " +  
                         "We bring you home, to the much-missed odours, to the familiar warmth " + 
-                        "of mom's cooking." ;
+                        "of mom's cooking. Check out our menu or get in touch!" ;
     intro.style.fontFamily = "'Marck Script', cursive";
-    document.querySelector("#content").appendChild(intro);
+    intro.style.width = "60%";
+    content.appendChild(intro);
+
+    let introImage = document.createElement("div");
+    const myImg = new Image();
+    myImg.src = Atithi;
+    introImage.appendChild(myImg);
+    content.appendChild(myImg);
 }
 
 export {createContent};
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
             {
                 test : /\.css$/i,
                 use : ['style-loader', 'css-loader'],
+            },
+            {
+                test : /\.(png|jpg|jpeg|gif|bmp|svg)$/i,
+                type : 'asset/resource'
             }
         ]
     }
