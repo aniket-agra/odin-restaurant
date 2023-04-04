06756d0ffc274cb8480e86d2820ce153d81b6f5c
Aniket Agrawal <aniket.mpa@gmail.com>
2023-04-04 21:29:06 +0200

    Add menu items
    Use a loop to loop over all items in menu. Ideally, should be able
    to read image filenames and descriptions from a data file.

--- src/menu-page.js
+++ src/menu-page.js
@@ -4,10 +4,38 @@ import "./menu-page.css";
 
 function createContent () {
     let content = document.querySelector("#content");
-    
+    content.style.paddingTop = "5%";
+
     let element = document.createElement("div");
+    element.style.display = "grid";
+    element.style.gridTemplateColumns = "1";
+    element.style.rowGap = "1rem";
     
-    
+    let num_items = 5;
+    for (let i = 0; i < num_items; i++) {
+        let item = document.createElement("div");
+        item.style.display = "flex";
+        item.style.justifyContent = "space-between";
+
+        let itemPic = new Image(200, 200);
+        itemPic.src = null;
+
+        let itemDesc = document.createElement("div");
+        itemDesc.textContent = "Some description of the dish";
+        itemDesc.style.alignSelf = "center";
+
+        if (i % 2 === 0) {
+            item.appendChild(itemPic);
+            item.appendChild(itemDesc);
+        }
+        else {
+            item.appendChild(itemDesc);
+            item.appendChild(itemPic);
+        }
+
+        element.appendChild(item);
+    }
+    content.appendChild(element);
 }
 
 export {createContent};
